b4af5db43033202ba1a0500ba483884d0b6aa1b3
Juan Carrión <hola@juancrrn.io>
2023-08-12 23:57:13 +0200

    Crear módulo REST y corregir paquete

--- code/boot/src/main/kotlin/io/juancrrn/balancerbankingconnector/BalancerBankingConnectorApplication.kt
+++ code/boot/src/main/kotlin/io/juancrrn/balancerbankingconnector/BalancerBankingConnectorApplication.kt
@@ -1,4 +1,4 @@
-package com.docline.prescriptionasproduct
+package io.juancrrn.balancerbankingconnector
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
